ccfb795de12cf0002776b2720daaca19bea0cbf7
Bill Decker <bill@concreteinteractive.com>
2012-10-25 16:05:43 -0400

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebSite::Application.config.secret_token = '9209f1426b34e42957c00143bf20c10d0ded9f5c7c2a367a364877b260479e9f2907bdb79bfb69a68edbf7e4a4576929be461ecb2721a45bdc589f38c886ead5'
